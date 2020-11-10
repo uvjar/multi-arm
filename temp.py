39878b6dfa07fa67f84c0d9725622aade4bb3e7c
caop <820672172@qq.com>
2020-11-10 16:43:48 +0000

    Update temp.py

--- temp.py
+++ temp.py
@@ -15,24 +15,23 @@ def main():
 		f_movies = np.load(path+"items.npy")
 		print("Got items... "); sys.stdout.flush()
 	else:
-		f_users = np.loadtxt(open(path+"users.txt", "r"), dtype=np.float32)
+		f_users = np.loadtxt(open(path+"users.txt", "r"), dtype=np.int32)
 		print("Got users... "); sys.stdout.flush()
 		np.save(path+"users.npy",f_users)
 		f_ratings = np.loadtxt(open(path+"ratings.txt", "r"), dtype=np.float32)
 		print("Got ratings... "); sys.stdout.flush()
 		np.save(path+"ratings.npy",f_ratings)
-		f_movies = np.loadtxt(open(path+"items.txt", "r"), dtype=np.float32)
+		f_movies = np.loadtxt(open(path+"items.txt", "r"), dtype=np.int32)
 		print("Got items... "); sys.stdout.flush()
 		np.save(path+"items.npy",f_movies)
 
-	R=sp.coo_matrix((f_ratings, (f_users, f_movies)), dtype=np.float32);R=sp.csc_matrix(R)
 	R = sp.coo_matrix((f_ratings, (f_users, f_movies)), dtype=np.float32)
-	R=sp.csc_matrix(R);
-	R=R[:,np.diff(R.indptr)>0];
+	R = sp.csc_matrix(R);
+	R = R[:,np.diff(R.indptr)>0];
 	print("number of columns "+str(R.indptr.size-1))
-	R=sp.csr_matrix(R)
+	R = sp.csr_matrix(R)
 	R = R[np.diff(R.indptr)>1]
-	R=sp.csr_matrix(R)
+	R = sp.csr_matrix(R)
 	print("number of rows "+str(R.indptr.size-1))
 	print("Got R.")
 
