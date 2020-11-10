699d11fe5437d5021e43205ff9eda64b749a60f8
caop <820672172@qq.com>
2020-11-10 16:38:53 +0000

    Update temp.py

--- temp.py
+++ temp.py
@@ -7,23 +7,23 @@ import numpy as np
 import scipy.sparse as sp
 path="data/"
 def main():
-	print("Retrieving R from files... ", end=""); sys.stdout.flush()
+	print("Retrieving R from files... "); sys.stdout.flush()
 	if os.path.isfile(path+"users.npy"):
 		f_users = np.load(path+"users.npy")
-		print("Got users... ", end=""); sys.stdout.flush()
+		print("Got users... "); sys.stdout.flush()
 		f_ratings = np.load(path+"ratings.npy")
-		print("Got ratings... ", end=""); sys.stdout.flush()
+		print("Got ratings... "); sys.stdout.flush()
 		f_movies = np.load(path+"items.npy")
-		print("Got items... ", end=""); sys.stdout.flush()
+		print("Got items... "); sys.stdout.flush()
 	else:
 		f_users = np.loadtxt(open(path+"users.txt", "r"), dtype=np.float32)
-		print("Got users... ", end=""); sys.stdout.flush()
+		print("Got users... "); sys.stdout.flush()
 		np.save(path+"users.npy",f_users)
 		f_ratings = np.loadtxt(open(path+"ratings.txt", "r"), dtype=np.float32)
-		print("Got ratings... ", end=""); sys.stdout.flush()
+		print("Got ratings... "); sys.stdout.flush()
 		np.save(path+"ratings.npy",f_ratings)
 		f_movies = np.loadtxt(open(path+"items.txt", "r"), dtype=np.float32)
-		print("Got items... ", end=""); sys.stdout.flush()
+		print("Got items... "); sys.stdout.flush()
 		np.save(path+"items.npy",f_movies)
 
 	R=sp.coo_matrix((f_ratings, (f_users, f_movies)), dtype=np.float32);R=sp.csc_matrix(R)
